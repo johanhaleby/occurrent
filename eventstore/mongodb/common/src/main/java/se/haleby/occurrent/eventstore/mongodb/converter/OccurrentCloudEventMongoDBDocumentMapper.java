9528b39f898a746137546f8e601bdcaf2dd0bfdb
Johan Haleby <johan.haleby@gmail.com>
2020-07-26 16:30:27 +0200

    Converting "time" to date in MongoDB in OccurrentCloudEventMongoDBDocumentMapper

--- eventstore/mongodb/common/src/main/java/se/haleby/occurrent/eventstore/mongodb/converter/OccurrentCloudEventMongoDBDocumentMapper.java
+++ eventstore/mongodb/common/src/main/java/se/haleby/occurrent/eventstore/mongodb/converter/OccurrentCloudEventMongoDBDocumentMapper.java
@@ -4,15 +4,38 @@ import io.cloudevents.CloudEvent;
 import io.cloudevents.core.format.EventFormat;
 import org.bson.Document;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.time.ZoneOffset.UTC;
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 import static se.haleby.occurrent.cloudevents.OccurrentCloudEventExtension.STREAM_ID;
 
 public class OccurrentCloudEventMongoDBDocumentMapper {
+
+    static DateTimeFormatter RFC_3339_DATE_TIME_FORMATTER = new DateTimeFormatterBuilder()
+            .append(ISO_LOCAL_DATE_TIME)
+            .optionalStart()
+            .appendOffset("+HH:MM", "Z")
+            .optionalEnd()
+            .toFormatter();
 
     public static Document convertToDocument(EventFormat eventFormat, String streamId, CloudEvent cloudEvent) {
         byte[] bytes = eventFormat.serialize(cloudEvent);
         String serializedAsString = new String(bytes, UTF_8);
         Document cloudEventDocument = Document.parse(serializedAsString);
+
+        // Convert date string to a date in order to be able to perform date/time queries
+        // on the "time" property name
+        ZonedDateTime time = cloudEvent.getTime();
+        if (time != null) {
+            Date date = Date.from(time.toInstant());
+            cloudEventDocument.put("time", date);
+        }
+
         cloudEventDocument.put(STREAM_ID, streamId);
         return cloudEventDocument;
     }
@@ -20,6 +43,14 @@ public class OccurrentCloudEventMongoDBDocumentMapper {
     public static CloudEvent convertToCloudEvent(EventFormat eventFormat, Document cloudEventDocument) {
         Document document = new Document(cloudEventDocument);
         document.remove("_id");
+        Date time = document.getDate("time");
+
+        if (time != null) {
+            ZonedDateTime zonedDateTime = ZonedDateTime.ofInstant(time.toInstant(), UTC);
+            String format = RFC_3339_DATE_TIME_FORMATTER.format(zonedDateTime);
+            document.put("time", format);
+        }
+
         String eventJsonString = document.toJson();
         byte[] eventJsonBytes = eventJsonString.getBytes(UTF_8);
         return eventFormat.deserialize(eventJsonBytes);
--- eventstore/mongodb/common/src/test/java/se/haleby/occurrent/eventstore/mongodb/converter/OccurrentCloudEventMongoDBDocumentMapperTest.java
+++ eventstore/mongodb/common/src/test/java/se/haleby/occurrent/eventstore/mongodb/converter/OccurrentCloudEventMongoDBDocumentMapperTest.java
@@ -8,21 +8,28 @@ import io.cloudevents.jackson.JsonFormat;
 import org.bson.Document;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import se.haleby.occurrent.cloudevents.OccurrentCloudEventExtension;
 
 import java.net.URI;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.time.ZoneOffset.UTC;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.entry;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static se.haleby.occurrent.eventstore.mongodb.converter.OccurrentCloudEventMongoDBDocumentMapper.RFC_3339_DATE_TIME_FORMATTER;
 
-@SuppressWarnings("ConstantConditions")
 class OccurrentCloudEventMongoDBDocumentMapperTest {
 
     private EventFormat eventFormat;
@@ -36,7 +43,7 @@ class OccurrentCloudEventMongoDBDocumentMapperTest {
     @Test
     void converts_cloud_event_to_document_with_expected_values() {
         // Given
-        ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.of(2020, 7, 26, 9, 13, 3), UTC);
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(LocalDateTime.of(2020, 7, 26, 9, 13, 3, 223_000000), UTC);
 
         CloudEvent cloudEvent = new CloudEventBuilder()
                 .withSubject("subject")
@@ -54,7 +61,7 @@ class OccurrentCloudEventMongoDBDocumentMapperTest {
         assertAll(
                 () -> assertThat(document.getString("subject")).isEqualTo("subject"),
                 () -> assertThat(document.getString("type")).isEqualTo("type"),
-                () -> assertThat(document.getString("time")).isEqualTo("2020-07-26T09:13:03Z"),
+                () -> assertThat(document.getDate("time")).isEqualTo(toRfc3339Date(zonedDateTime)),
                 () -> assertThat(document.getString("source")).isEqualTo("urn:name"),
                 () -> assertThat(document.getString("id")).isEqualTo("id"),
                 () -> assertThat(document.get("data", Map.class)).containsOnly(entry("name", "hello")),
@@ -62,14 +69,13 @@ class OccurrentCloudEventMongoDBDocumentMapperTest {
         );
     }
 
-    @SuppressWarnings("unchecked")
     @Test
     void converts_document_to_cloud_event_and_remove_mongo_id() {
         // Given
         Document document = new Document(new HashMap<String, Object>() {{
             put("subject", "subject");
             put("type", "type");
-            put("time", "2020-07-26T09:13:03Z");
+            put("time", toRfc3339Date("2020-07-26T09:13:03Z"));
             put("source", "urn:name");
             put("id", "id");
             put("_id", "mongodb");
@@ -96,5 +102,113 @@ class OccurrentCloudEventMongoDBDocumentMapperTest {
                 .build();
 
         assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void converts_document_to_cloud_event_when_time_is_specified_with_millis() {
+        // Given
+        Document document = new Document(new HashMap<String, Object>() {{
+            put("subject", "subject");
+            put("type", "type");
+            put("time", toRfc3339Date("2020-07-26T09:13:03.234Z"));
+            put("source", "urn:name");
+            put("id", "id");
+            put("_id", "mongodb");
+            put("data", new HashMap<String, Object>() {{
+                put("name", "hello");
+            }});
+            put("datacontenttype", "application/json");
+            put("specversion", "1.0");
+            put("streamId", "streamId");
+        }});
+
+        // When
+        CloudEvent actual = OccurrentCloudEventMongoDBDocumentMapper.convertToCloudEvent(eventFormat, document);
+
+        // Then
+        assertThat(actual.getTime()).isEqualTo(ZonedDateTime.of(LocalDateTime.of(2020, 7, 26, 9, 13, 3, 234_000000), UTC));
+    }
+
+    @Test
+    void converts_document_to_cloud_event_when_time_is_specified_with_timezone_that_is_plus_utc() {
+        // Given
+        Document document = new Document(new HashMap<String, Object>() {{
+            put("subject", "subject");
+            put("type", "type");
+            put("time", toRfc3339Date("2020-07-26T09:13:03+02:00"));
+            put("source", "urn:name");
+            put("id", "id");
+            put("_id", "mongodb");
+            put("data", new HashMap<String, Object>() {{
+                put("name", "hello");
+            }});
+            put("datacontenttype", "application/json");
+            put("specversion", "1.0");
+            put("streamId", "streamId");
+        }});
+
+        // When
+        CloudEvent actual = OccurrentCloudEventMongoDBDocumentMapper.convertToCloudEvent(eventFormat, document);
+
+        // Then
+        assertThat(actual.getTime()).isEqualTo(ZonedDateTime.of(LocalDateTime.of(2020, 7, 26, 9, 13, 3), ZoneId.of("CET")));
+    }
+
+    @Test
+    void converts_document_to_cloud_event_when_time_is_specified_with_timezone_that_is_minus_utc() {
+        // Given
+        Document document = new Document(new HashMap<String, Object>() {{
+            put("subject", "subject");
+            put("type", "type");
+            put("time", toRfc3339Date("2020-07-26T09:13:03-02:00"));
+            put("source", "urn:name");
+            put("id", "id");
+            put("_id", "mongodb");
+            put("data", new HashMap<String, Object>() {{
+                put("name", "hello");
+            }});
+            put("datacontenttype", "application/json");
+            put("specversion", "1.0");
+            put("streamId", "streamId");
+        }});
+
+        // When
+        CloudEvent actual = OccurrentCloudEventMongoDBDocumentMapper.convertToCloudEvent(eventFormat, document);
+
+        // Then
+        assertThat(actual.getTime()).isEqualTo(ZonedDateTime.of(LocalDateTime.of(2020, 7, 26, 9, 13, 3), ZoneOffset.of("-02:00")));
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("rfc3339Data")
+    void rfc3339_date_time_conversion_test(String dateTimeString, ZonedDateTime expected) {
+        // When
+        ZonedDateTime actual = ZonedDateTime.from(RFC_3339_DATE_TIME_FORMATTER.parse(dateTimeString));
+
+        // Then
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    private static Stream<Arguments> rfc3339Data() {
+        return Stream.of(
+                Arguments.of("2007-05-01T15:43:26+07:00", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26), ZoneOffset.of("+07:00"))),
+                Arguments.of("2007-05-01T15:43:26.3+07:00", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26, 300_000_000), ZoneOffset.of("+07:00"))),
+                Arguments.of("2007-05-01T15:43:26.3452+07:00", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26, 345_200_000), ZoneOffset.of("+07:00"))),
+                Arguments.of("2007-05-01T15:43:26-07:00", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26), ZoneOffset.of("-07:00"))),
+                Arguments.of("2007-05-01T15:43:26.3-07:00", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26, 300_000_000), ZoneOffset.of("-07:00"))),
+                Arguments.of("2007-05-01T15:43:26.3452-07:00", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26, 345_200_000), ZoneOffset.of("-07:00"))),
+                Arguments.of("2007-05-01T15:43:26.3452Z", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26, 345_200_000), UTC)),
+                Arguments.of("2007-05-01T15:43:26.3Z", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26, 300_000_000), UTC)),
+                Arguments.of("2007-05-01T15:43:26Z", ZonedDateTime.of(LocalDateTime.of(2007, 5, 1, 15, 43, 26), UTC))
+        );
+    }
+
+    private static Date toRfc3339Date(ZonedDateTime zonedDateTime) {
+        return Date.from(zonedDateTime.toInstant());
+    }
+
+    private static Date toRfc3339Date(String rfc3339FormattedString) {
+        return toRfc3339Date(ZonedDateTime.from(RFC_3339_DATE_TIME_FORMATTER.parse(rfc3339FormattedString)));
     }
 }
