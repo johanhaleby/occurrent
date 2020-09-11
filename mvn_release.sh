1795a1b0b7ddd18cdf75c31662dcff42ea98f785
Johan Haleby <johan.haleby@gmail.com>
2020-09-11 14:59:16 +0200

    Fixing script

--- mvn_release.sh
+++ mvn_release.sh
@@ -2,7 +2,7 @@
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to release Occurrent $releaseVersion"
 
-mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag=occurrent-"${releaseVersion}" -DreleaseVersion="${releaseVersion}" &&
+mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" &&
 mvn release:perform -Prelease
 
 echo "Maven release of Occurrent $releaseVersion completed successfully"
