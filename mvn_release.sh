d1faca7f986840884f900d1460c20f0c792a9805
Johan Haleby <johan.haleby@gmail.com>
2020-09-11 14:58:08 +0200

    Added maven release script

---
+++ mvn_release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -p "Enter the version to release: " releaseVersion
+echo "Starting to release Occurrent $releaseVersion"
+
+mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag=occurrent-"${releaseVersion}" -DreleaseVersion="${releaseVersion}" &&
+mvn release:perform -Prelease
+
+echo "Maven release of Occurrent $releaseVersion completed successfully"
+
