4bbb4b9b0b76655a260c67620fe30697c9bf59a4
Johan Haleby <johan.haleby@gmail.com>
2020-09-26 14:16:02 +0200

    Switching to java 8 in script

--- mvn_release.sh
+++ mvn_release.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+sdk use java 8.0.222.hs-adpt
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to release Occurrent $releaseVersion"
 
 mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" && \
 mvn release:perform -Prelease && \
 echo "Maven release of Occurrent $releaseVersion completed successfully"
-
