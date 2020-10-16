813cc38236866fe2b637892fb92793eebdea3e29
Johan Haleby <johan.haleby@gmail.com>
2020-10-16 10:29:29 +0200

    Introducing DomainEventQueries

--- example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/features/game/queries/FindGameByGameIdQuery.kt
+++ example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/features/game/queries/FindGameByGameIdQuery.kt
@@ -1,20 +1,16 @@
 package org.occurrent.example.domain.wordguessinggame.mongodb.spring.blocking.features.game.queries
 
-import org.occurrent.eventstore.api.blocking.EventStoreQueries
-import org.occurrent.example.domain.wordguessinggame.mongodb.spring.blocking.infrastructure.CloudEventConverter
-import org.occurrent.example.domain.wordguessinggame.readmodel.game.GameReadModel
+import org.occurrent.example.domain.wordguessinggame.mongodb.spring.blocking.infrastructure.DomainEventQueries
 import org.occurrent.example.domain.wordguessinggame.readmodel.game.AssembleGameReadModelFromDomainEvents
-import org.occurrent.filter.Filter.streamId
+import org.occurrent.example.domain.wordguessinggame.readmodel.game.GameReadModel
 import org.springframework.stereotype.Component
 import java.util.*
-import kotlin.streams.asSequence
 
 @Component
-class FindGameByGameIdQuery(private val eventStoreQueries: EventStoreQueries, private val cloudEventConverter: CloudEventConverter) {
+class FindGameByGameIdQuery(private val domainEvents: DomainEventQueries) {
 
     fun execute(gameId: UUID): GameReadModel? =
-            eventStoreQueries.query(streamId(gameId.toString())).asSequence()
-                    .map(cloudEventConverter::toDomainEvent)
+            domainEvents.forGame(gameId)
                     .fold(AssembleGameReadModelFromDomainEvents(), AssembleGameReadModelFromDomainEvents::applyEvent)
                     .gameReadModel
 }
---
+++ example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/infrastructure/DomainEventQueries.kt
@@ -0,0 +1,20 @@
+package org.occurrent.example.domain.wordguessinggame.mongodb.spring.blocking.infrastructure
+
+import org.occurrent.eventstore.api.blocking.EventStoreQueries
+import org.occurrent.example.domain.wordguessinggame.event.DomainEvent
+import org.occurrent.example.domain.wordguessinggame.writemodel.game.GameId
+import org.occurrent.filter.Filter.streamId
+import org.springframework.stereotype.Component
+import kotlin.streams.asSequence
+
+
+/**
+ * Simple wrapper around [EventStoreQueries] that provides an interface that deals with domain events instead of cloud events
+ */
+@Component
+class DomainEventQueries(private val eventStoreQueries: EventStoreQueries, private val cloudEventConverter: CloudEventConverter) {
+
+    fun forGame(gameId: GameId): Sequence<DomainEvent> =
+            eventStoreQueries.query(streamId(gameId.toString())).asSequence()
+                    .map(cloudEventConverter::toDomainEvent)
+}
