b0eb32a9fac04c058fed71bcba39cc00cf1206b2
Johan Haleby <johan.haleby@gmail.com>
2020-10-16 12:27:01 +0200

    Renamed FindGameByGameIdQuery to FindGameByIdQuery

--- example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/features/game/queries/FindGameByIdQuery.kt
+++ example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/features/game/queries/FindGameByIdQuery.kt
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
-class FindGameByGameIdQuery(private val domainEvents: DomainEventQueries) {
+class FindGameByIdQuery(private val domainEvents: DomainEventQueries) {
 
     fun execute(gameId: UUID): GameReadModel? =
             domainEvents.forGame(gameId)
