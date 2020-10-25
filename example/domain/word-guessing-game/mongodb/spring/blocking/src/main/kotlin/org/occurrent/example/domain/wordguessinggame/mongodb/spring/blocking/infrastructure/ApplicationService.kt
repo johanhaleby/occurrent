1d478c399dbc4c8f64b7cb34752454126c5b841d
Johan Haleby <johan.haleby@gmail.com>
2020-10-25 14:13:26 +0100

    Added @Transaction to AS

--- example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/infrastructure/ApplicationService.kt
+++ example/domain/word-guessing-game/mongodb/spring/blocking/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/blocking/infrastructure/ApplicationService.kt
@@ -6,18 +6,31 @@ import org.occurrent.eventstore.api.WriteConditionNotFulfilledException
 import org.occurrent.example.domain.wordguessinggame.event.DomainEvent
 import org.springframework.retry.annotation.Backoff
 import org.springframework.retry.annotation.Retryable
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 interface ApplicationService {
-    fun execute(streamId: UUID, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>)
-    fun execute(streamId: String, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>)
+    fun execute(streamId: UUID, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>, sideEffects: ((Sequence<DomainEvent>) -> Unit)? = null)
+    fun execute(streamId: String, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>, sideEffects: ((Sequence<DomainEvent>) -> Unit)? = null)
+
+    fun execute(streamId: UUID, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>) =
+            execute(streamId, functionThatCallsDomainModel, null)
+
+    fun execute(streamId: String, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>) =
+            execute(streamId, functionThatCallsDomainModel, null)
 }
 
 open class RetryableApplicationService constructor(private val applicationService: GenericApplicationService<DomainEvent>) : ApplicationService {
 
+    @Transactional
     @Retryable(include = [WriteConditionNotFulfilledException::class], maxAttempts = 5, backoff = Backoff(delay = 100, multiplier = 2.0, maxDelay = 1000))
-    override fun execute(streamId: UUID, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>) = applicationService.execute(streamId, functionThatCallsDomainModel)
+    override fun execute(streamId: UUID,
+                         functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>,
+                         sideEffects: ((Sequence<DomainEvent>) -> Unit)?) = applicationService.execute(streamId, functionThatCallsDomainModel, sideEffects)
 
+    @Transactional
     @Retryable(include = [WriteConditionNotFulfilledException::class], maxAttempts = 5, backoff = Backoff(delay = 100, multiplier = 2.0, maxDelay = 1000))
-    override fun execute(streamId: String, functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>) = applicationService.execute(streamId, functionThatCallsDomainModel)
+    override fun execute(streamId: String,
+                         functionThatCallsDomainModel: (Sequence<DomainEvent>) -> Sequence<DomainEvent>,
+                         sideEffects: ((Sequence<DomainEvent>) -> Unit)?) = applicationService.execute(streamId, functionThatCallsDomainModel, sideEffects)
 }
