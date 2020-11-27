4e4afe49918eebc0f821dd79e37f01c9d32e35d6
Johan Haleby <johan.haleby@gmail.com>
2020-11-27 13:41:03 +0100

    Minor improvement

--- example/domain/word-guessing-game/model/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/writemodel/GameLogic.kt
+++ example/domain/word-guessing-game/model/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/writemodel/GameLogic.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Johan Haleby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.occurrent.example.domain.wordguessinggame.writemodel
 
 import org.occurrent.example.domain.wordguessinggame.event.*
@@ -62,7 +78,7 @@ private data class Ongoing(val gameId: GameId, val wordToGuess: String, val maxN
     fun isMaxNumberOfGuessesExceededForPlayer(playerId: PlayerId): Boolean = numberOfGuessesForPlayer(playerId) == maxNumberOfGuessesPerPlayer
     fun isLastGuessForPlayer(playerId: PlayerId): Boolean = numberOfGuessesForPlayer(playerId) + 1 == maxNumberOfGuessesPerPlayer
     fun isLastGuessForGame(): Boolean = guesses.size == maxNumberOfGuessesTotal - 1
-    fun isRightGuess(guessedWord: Word) = guessedWord.value.toUpperCase() == wordToGuess.toUpperCase()
+    fun isRightGuess(guessedWord: Word) = guessedWord.value.equals(wordToGuess, ignoreCase = true)
 }
 
 private object Ended : GameState()
