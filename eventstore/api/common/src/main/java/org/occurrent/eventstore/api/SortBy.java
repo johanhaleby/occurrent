d366d52150fbb1d3c39dfa7e6fcfd9ef2e57f851
Johan Haleby <johan.haleby@gmail.com>
2021-02-19 13:19:24 +0100

    Better javadoc for SortBy

--- eventstore/api/common/src/main/java/org/occurrent/eventstore/api/SortBy.java
+++ eventstore/api/common/src/main/java/org/occurrent/eventstore/api/SortBy.java
@@ -1,26 +1,61 @@
 package org.occurrent.eventstore.api;
 
+import io.cloudevents.core.v1.CloudEventV1;
+import org.occurrent.cloudevents.OccurrentCloudEventExtension;
+
 import java.util.*;
 
 import static io.cloudevents.core.v1.CloudEventV1.TIME;
 import static org.occurrent.cloudevents.OccurrentCloudEventExtension.STREAM_VERSION;
 
+/**
+ * A fluent API for specifying sort order for queries. For example:
+ * <p></p>
+ * <pre>
+ * var allEvents = eventStoreQueries.all(SortBy.time(DESCENDING));
+ * </pre>
+ */
 public abstract class SortBy {
     private SortBy() {
     }
 
+    /**
+     * Sort by natural order in the supplied direction. This is typically the insertion order,
+     * but it could also be undefined for certain datastores.
+     *
+     * @param direction The direction
+     * @return A new instance of {@link SortBy}.
+     */
     public static Natural natural(SortDirection direction) {
         return new Natural(direction);
     }
 
+    /**
+     * Sort by time in the supplied direction.
+     *
+     * @param direction The direction
+     * @return A new instance of {@link SortBy}.
+     */
     public static SingleField time(SortDirection direction) {
         return field(TIME, direction);
     }
 
+    /**
+     * Sort by {@value OccurrentCloudEventExtension#STREAM_VERSION} in the supplied direction.
+     *
+     * @param direction The direction
+     * @return A new instance of {@link SortBy}.
+     */
     public static SingleField streamVersion(SortDirection direction) {
         return field(STREAM_VERSION, direction);
     }
 
+    /**
+     * Sort by the given field and direction. See {@link CloudEventV1} for valid fields.
+     *
+     * @param direction The direction
+     * @return A new instance of {@link SortBy}.
+     */
     public static SingleField field(String fieldName, SortDirection direction) {
         return new SingleField(fieldName, direction);
     }
@@ -67,6 +102,12 @@ public abstract class SortBy {
             this.direction = direction;
         }
 
+        /**
+         * Combines this sorting step and with another step such that the latter is applied only when the former considered values equal.
+         *
+         * @param direction The direction
+         * @return A new instance of {@link SortBy}.
+         */
         public SortBy thenNatural(SortDirection direction) {
             return then(new Natural(direction));
         }
@@ -114,6 +155,12 @@ public abstract class SortBy {
             this.steps = Collections.unmodifiableList(steps);
         }
 
+        /**
+         * Combines this sorting step and with another step such that the latter is applied only when the former considered values equal.
+         *
+         * @param direction The direction
+         * @return A new instance of {@link SortBy}.
+         */
         public SortBy thenNatural(SortDirection direction) {
             return then(new Natural(direction));
         }
@@ -174,6 +221,12 @@ public abstract class SortBy {
             return then(field(fieldName, direction));
         }
 
+        /**
+         * Combines this sorting step and with another step such that the latter is applied only when the former considered values equal.
+         *
+         * @param next The next step.
+         * @return A new instance of {@link SortBy}.
+         */
         public abstract MultipleSortSteps then(ComposableSortStep next);
     }
 }
