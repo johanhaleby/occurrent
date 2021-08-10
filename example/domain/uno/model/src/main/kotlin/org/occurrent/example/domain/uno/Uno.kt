e3317b0e369d1e41abcd2aec6c4c9c83efc38c81
Johan Haleby <johan.haleby@gmail.com>
2021-08-10 11:39:19 +0200

    Throwing exception instead of returning current state on invalid state transations in Uno example

--- example/domain/uno/model/src/main/kotlin/org/occurrent/example/domain/uno/Uno.kt
+++ example/domain/uno/model/src/main/kotlin/org/occurrent/example/domain/uno/Uno.kt
@@ -109,12 +109,13 @@ private sealed class State
 private object NotStarted : State()
 private data class Started(val gameId: GameId, val turn: Turn, val topCard: Card) : State()
 
+private inline fun <reified ExpectedState : State> invalidState(actualState: State): Nothing = throw IllegalStateException("Invalid state: Expecting ${ExpectedState::class.simpleName}, was ${actualState::class.simpleName}")
 private fun Sequence<Event>.evolve(): State = fold<Event, State>(NotStarted) { currentState, event ->
     when (event) {
         is GameStarted -> Started(event.gameId, Turn(event.firstPlayerId, event.playerCount, Clockwise), event.firstCard)
-        is CardPlayed -> if (currentState is Started) currentState.copy(turn = currentState.turn.setPlayer(event.nextPlayerId), topCard = event.card) else currentState
+        is CardPlayed -> if (currentState is Started) currentState.copy(turn = currentState.turn.setPlayer(event.nextPlayerId), topCard = event.card) else invalidState<Started>(currentState)
         is PlayerPlayedAtWrongTurn -> currentState
         is PlayerPlayedWrongCard -> currentState
-        is DirectionChanged -> if (currentState is Started) currentState.copy(turn = currentState.turn.setDirection(event.direction)) else currentState
+        is DirectionChanged -> if (currentState is Started) currentState.copy(turn = currentState.turn.setDirection(event.direction)) else invalidState<Started>(currentState)
     }
 }
