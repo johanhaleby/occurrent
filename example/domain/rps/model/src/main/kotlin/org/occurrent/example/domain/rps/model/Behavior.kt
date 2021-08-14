e0b398f277bdf4c9d4f154c80037d20abad5e855
Johan Haleby <johan.haleby@gmail.com>
2021-08-14 13:56:43 +0200

    Started working on RPS

---
+++ example/domain/rps/model/src/main/kotlin/org/occurrent/example/domain/rps/model/Behavior.kt
@@ -0,0 +1,126 @@
+/*
+ *
+ *  Copyright 2021 Johan Haleby
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.occurrent.example.domain.rps.model
+
+import kotlinx.collections.immutable.PersistentList
+import kotlinx.collections.immutable.persistentListOf
+import org.occurrent.example.domain.rps.model.GameState.*
+
+fun handle(events: Sequence<GameEvent>, cmd: CreateGameCommand): Sequence<GameEvent> = when (events.evolve()) {
+    is CurrentState -> throw GameCannotBeCreatedMoreThanOnce()
+    else -> {
+        val (gameId, timestamp, creator, numberOfRounds) = cmd
+        sequenceOf(GameCreated(gameId, timestamp, creator, numberOfRounds))
+    }
+}
+
+fun handle(events: Sequence<GameEvent>, cmd: MakeMoveCommand): Sequence<GameEvent> = when (val currentState = events.evolve()) {
+    is CurrentState -> makeMove(currentState, cmd)
+    else -> throw GameDoesNotExist()
+}
+
+private fun makeMove(state: CurrentState, cmd: MakeMoveCommand): Sequence<GameEvent> {
+    val (timestamp, playerId, move) = cmd
+    val gameId = state.gameId
+    return when (state.state) {
+        WaitingForFirstPlayer -> sequenceOf(FirstPlayerJoinedGame(state.gameId, timestamp, playerId)) + startNewRound(state, timestamp, PlayerMove(playerId, move))
+        WaitingForSecondPlayer -> if (cmd.playerId == state.firstPlayer) throw CannotJoinTheGameTwice() else sequenceOf(
+            SecondPlayerJoinedGame(gameId, timestamp, playerId),
+            GameStarted(gameId, timestamp, playerId),
+            MoveMade(gameId, timestamp, playerId, move)
+        )
+        Ongoing -> TODO()
+        Ended -> throw CannotMakeMoveBecauseGameEnded()
+    }
+}
+
+private fun startNewRound(state: CurrentState, timestamp: Timestamp, playerMove: PlayerMove): Sequence<GameEvent> {
+    val currentNumberOfRounds = state.rounds.size
+    return if (currentNumberOfRounds < state.numberOfRounds.value) {
+        sequenceOf(
+            RoundStarted(state.gameId, timestamp, RoundNumber(currentNumberOfRounds.inc())),
+            MoveMade(state.gameId, timestamp, playerMove.playerId, playerMove.move)
+        )
+    } else {
+        throw IllegalStateException("Cannot start round since it would exceed ${state.numberOfRounds.value}")
+    }
+}
+
+private fun makeMove(state: CurrentState, timestamp: Timestamp, playerMove: PlayerMove) {
+    
+}
+
+// Internal models
+private enum class GameState {
+    WaitingForFirstPlayer, WaitingForSecondPlayer, Ongoing, Ended
+}
+
+private data class PlayerMove(val playerId: PlayerId, val move: Move)
+
+private enum class RoundState {
+    Started, MoveMade, Tied, Won
+}
+
+private data class Round(val state: RoundState, val moves: PersistentList<PlayerMove> = persistentListOf(), val winner: PlayerId? = null)
+
+private fun Round.play(player: PlayerId, move: Move) {
+
+}
+
+// Evolving from events
+private data class CurrentState(
+    val gameId: GameId, val state: GameState, val numberOfRounds: NumberOfRounds, val firstPlayer: PlayerId? = null, val secondPlayer: PlayerId? = null,
+    val rounds: PersistentList<Round> = persistentListOf()
+)
+
+private fun Sequence<GameEvent>.evolve(): CurrentState? = fold<GameEvent, CurrentState?>(null) { currentState, e ->
+    when (e) {
+        is GameCreated -> CurrentState(gameId = e.game, state = WaitingForFirstPlayer, numberOfRounds = e.numberOfRounds)
+        is FirstPlayerJoinedGame -> currentState!!.copy(state = WaitingForSecondPlayer, firstPlayer = e.player)
+        is SecondPlayerJoinedGame -> currentState!!.copy(secondPlayer = e.player)
+        is GameStarted -> currentState!!.copy(state = Ongoing)
+        is RoundStarted -> currentState!!.copy(rounds = currentState.rounds.add(Round(RoundState.Started)))
+        is MoveMade -> currentState!!.updateRound(RoundNumber.unsafe(currentState.rounds.size)) {
+            copy(
+                moves = moves.add(PlayerMove(e.player, e.move)),
+                state = RoundState.MoveMade
+            )
+        }
+        is RoundWon -> currentState!!.updateRound(e.roundNumber) {
+            copy(
+                winner = e.winner,
+                state = RoundState.Won
+            )
+        }
+        is RoundTied -> currentState!!.updateRound(e.roundNumber) {
+            copy(state = RoundState.Tied)
+        }
+        is RoundEnded -> currentState
+        is GameTied -> currentState
+        is GameWon -> currentState
+        is GameEnded -> currentState!!.copy(state = Ended)
+    }
+}
+
+private fun CurrentState.updateRound(roundNumber: RoundNumber, fn: Round.() -> Round): CurrentState = copy(rounds = rounds.updateRound(roundNumber, fn))
+
+private fun PersistentList<Round>.updateRound(roundNumber: RoundNumber, fn: Round.() -> Round): PersistentList<Round> {
+    val roundIndex = roundNumber.value - 1
+    val updatedRound = fn(get(roundIndex))
+    return set(roundIndex, updatedRound)
+}
---
+++ example/domain/rps/model/src/main/kotlin/org/occurrent/example/domain/rps/model/Events.kt
@@ -0,0 +1,37 @@
+/*
+ *
+ *  Copyright 2021 Johan Haleby
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.occurrent.example.domain.rps.model
+
+sealed interface GameEvent {
+    val game: GameId
+    val timestamp: Timestamp
+}
+
+data class FirstPlayerJoinedGame(override val game: GameId, override val timestamp: Timestamp, val player: PlayerId) : GameEvent
+data class SecondPlayerJoinedGame(override val game: GameId, override val timestamp: Timestamp, val player: PlayerId) : GameEvent
+
+data class GameCreated(override val game: GameId, override val timestamp: Timestamp, val createdBy: GameCreatorId, val numberOfRounds: NumberOfRounds) : GameEvent
+data class GameStarted(override val game: GameId, override val timestamp: Timestamp, val startedBy: PlayerId) : GameEvent
+data class GameEnded(override val game: GameId, override val timestamp: Timestamp) : GameEvent
+data class GameTied(override val game: GameId, override val timestamp: Timestamp) : GameEvent
+data class GameWon(override val game: GameId, override val timestamp: Timestamp, val winner: PlayerId) : GameEvent
+data class MoveMade(override val game: GameId, override val timestamp: Timestamp, val player: PlayerId, val move: Move) : GameEvent
+data class RoundStarted(override val game: GameId, override val timestamp: Timestamp, val roundNumber: RoundNumber) : GameEvent
+data class RoundEnded(override val game: GameId, override val timestamp: Timestamp, val roundNumber: RoundNumber) : GameEvent
+data class RoundWon(override val game: GameId, override val timestamp: Timestamp, val roundNumber: RoundNumber, val winner: PlayerId) : GameEvent
+data class RoundTied(override val game: GameId, override val timestamp: Timestamp, val roundNumber: RoundNumber) : GameEvent
