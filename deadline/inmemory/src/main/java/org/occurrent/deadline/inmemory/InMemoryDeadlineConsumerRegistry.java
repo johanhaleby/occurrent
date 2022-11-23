68b765ad1474ef2f7d64555cee5a0095e08ad30e
Johan Haleby <johan.haleby@gmail.com>
2022-11-23 14:40:07 +0100

    Added retry strategy and better config options to InMemoryDeadlineConsumerRegistry

--- deadline/inmemory/src/main/java/org/occurrent/deadline/inmemory/InMemoryDeadlineConsumerRegistry.java
+++ deadline/inmemory/src/main/java/org/occurrent/deadline/inmemory/InMemoryDeadlineConsumerRegistry.java
@@ -20,11 +20,14 @@ package org.occurrent.deadline.inmemory;
 import org.occurrent.deadline.api.blocking.DeadlineConsumer;
 import org.occurrent.deadline.api.blocking.DeadlineConsumerRegistry;
 import org.occurrent.deadline.inmemory.internal.DeadlineData;
+import org.occurrent.retry.RetryStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.StringJoiner;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -38,24 +41,21 @@ public class InMemoryDeadlineConsumerRegistry implements DeadlineConsumerRegistr
     private volatile boolean running = true;
 
     public InMemoryDeadlineConsumerRegistry(BlockingDeque<DeadlineData> deadlineQueue) {
-        this(deadlineQueue, 500);
+        this(deadlineQueue, new Config());
     }
 
-    public InMemoryDeadlineConsumerRegistry(BlockingDeque<DeadlineData> deadlineQueue, long pollIntervalMillis) {
+    public InMemoryDeadlineConsumerRegistry(BlockingDeque<DeadlineData> deadlineQueue, Config config) {
         Objects.requireNonNull(deadlineQueue, "Deadline queue cannot be null");
-        if (pollIntervalMillis < 1) {
-            throw new IllegalArgumentException("Poll interval must be greater than zero");
-        }
         thread = new Thread(() -> {
             while (running) {
                 try {
-                    DeadlineData data = deadlineQueue.pollFirst(pollIntervalMillis, TimeUnit.MILLISECONDS);
+                    DeadlineData data = deadlineQueue.pollFirst(config.pollInterval, config.pollIntervalTimeUnit);
                     if (data != null) {
                         DeadlineConsumer<Object> deadlineConsumer = deadlineConsumers.get(data.category);
                         if (deadlineConsumer == null) {
                             log.warn("Failed to find a deadline consumer for category {}, will try again later.", data.category);
                         } else {
-                            deadlineConsumer.accept(data.id, data.category, data.deadline, data.data);
+                            config.retryStrategy.execute(() -> deadlineConsumer.accept(data.id, data.category, data.deadline, data.data));
                         }
                     }
                 } catch (InterruptedException e) {
@@ -100,6 +100,68 @@ public class InMemoryDeadlineConsumerRegistry implements DeadlineConsumerRegistr
             thread.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static class Config {
+        final long pollInterval;
+        final TimeUnit pollIntervalTimeUnit;
+        final RetryStrategy retryStrategy;
+
+        /**
+         * Create config with the following settings:
+         * <ol>
+         *     <li>Poll interval - 500 ms</li>
+         *     <li>RetryStrategy - Fixed every 1 second</li>
+         * </ol>
+         */
+        public Config() {
+            this(500, TimeUnit.MILLISECONDS, RetryStrategy.fixed(Duration.ofSeconds(1)));
+        }
+
+        public Config(long pollInterval, TimeUnit pollIntervalTimeUnit, RetryStrategy retryStrategy) {
+            if (pollInterval < 1) {
+                throw new IllegalArgumentException("pollInterval must be greater than zero");
+            }
+            Objects.requireNonNull(pollIntervalTimeUnit, "pollIntervalTimeUnit cannot be null");
+            Objects.requireNonNull(retryStrategy, RetryStrategy.class.getSimpleName() + " cannot be null");
+            this.pollInterval = pollInterval;
+            this.pollIntervalTimeUnit = pollIntervalTimeUnit;
+            this.retryStrategy = retryStrategy;
+        }
+
+        public Config pollIntervalMillis(long pollIntervalMillis) {
+            return new Config(pollIntervalMillis, TimeUnit.MILLISECONDS, retryStrategy);
+        }
+
+        public Config pollInterval(long pollIntervalMillis, TimeUnit timeUnit) {
+            return new Config(pollIntervalMillis, timeUnit, retryStrategy);
+        }
+
+        public Config retryStrategy(RetryStrategy retryStrategy) {
+            return new Config(pollInterval, pollIntervalTimeUnit, retryStrategy);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Config)) return false;
+            Config config = (Config) o;
+            return pollInterval == config.pollInterval && pollIntervalTimeUnit == config.pollIntervalTimeUnit && Objects.equals(retryStrategy, config.retryStrategy);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(pollInterval, pollIntervalTimeUnit, retryStrategy);
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", Config.class.getSimpleName() + "[", "]")
+                    .add("pollInterval=" + pollInterval)
+                    .add("pollIntervalTimeUnit=" + pollIntervalTimeUnit)
+                    .add("retryStrategy=" + retryStrategy)
+                    .toString();
         }
     }
 }
