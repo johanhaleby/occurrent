e59fb33aaf8f5b8fac8133ce69bdf555fb7438ba
Johan Haleby <johan.haleby@gmail.com>
2022-12-09 14:09:48 +0100

    [ci skip] Added support entering sonatype password beforehand

--- mvn_release.sh
+++ mvn_release.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "!!!!!DON'T FORGET TO SWITCH TO JAVA 17!!!!!"
 read -p "Enter the version to release: " releaseVersion
+read -s -p "Sonatype password: " sonatypePassword
 echo "Starting to release Occurrent $releaseVersion"
 
-mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" && \
+mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" -Dgpg.passphrase="${sonatypePassword}" && \
 mvn release:perform -Prelease && \
 echo "Maven release of Occurrent $releaseVersion completed successfully"
