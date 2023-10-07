fbd904a79b20e1800b66941a753e2573f556e9ad
Johan Haleby <johan.haleby@gmail.com>
2023-10-07 14:36:38 +0200

    [ci skip] Updating release script to also update version number for modules not included in release

--- mvn_release.sh
+++ mvn_release.sh
@@ -5,4 +5,10 @@ echo "Starting to release Occurrent $releaseVersion"
 
 mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" && \
 mvn release:perform -Prelease && \
+
+echo "Updating version number for modules that were not included in release build"
+snapshotVersionOfMainPom=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
+mvn versions:set -DnewVersion="${snapshotVersionOfMainPom}" -DprocessAllModules -DgenerateBackupPoms=false && \
+git commit -am "Updated version number for modules that were not included in release build" && \
+git push && \
 echo "Maven release of Occurrent $releaseVersion completed successfully"
