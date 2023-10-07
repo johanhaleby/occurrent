293297f5d1520dc1fa5bdeaec0588317dd4ec6cf
Johan Haleby <johan.haleby@gmail.com>
2023-10-07 15:00:23 +0200

    [ci skip] Fixed issues in release script

--- mvn_release.sh
+++ mvn_release.sh
@@ -3,12 +3,17 @@ echo "!!!!!DON'T FORGET TO SWITCH TO JAVA 17!!!!!"
 read -p "Enter the version to release: " releaseVersion
 echo "Starting to release Occurrent $releaseVersion"
 
+versionBeforeRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
+
 mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" && \
 mvn release:perform -Prelease && \
 
-echo "Updating version number for modules that were not included in release build"
-snapshotVersionOfMainPom=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
-mvn versions:set -DnewVersion="${snapshotVersionOfMainPom}" -DprocessAllModules -DgenerateBackupPoms=false && \
-git commit -am "Updated version number for modules that were not included in release build" && \
+echo "Release successful, will update version number for modules that were not included in release build."
+git pull --rebase && \
+versionAfterRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
+# We normalize versionAfterRelease by escaping dots with \. This is required for sed to work in find.
+normalizedVersionBeforeRelease=$(echo "${versionBeforeRelease}" | sed 's/\./\\./g') && \
+find . -name "pom.xml" -type f -exec sed -i "" "s/${normalizedVersionBeforeRelease}/${versionAfterRelease}/g" {} + && \
+git commit -am "Updated version number for modules that were not included in release build to ${versionAfterRelease}" && \
 git push && \
 echo "Maven release of Occurrent $releaseVersion completed successfully"
