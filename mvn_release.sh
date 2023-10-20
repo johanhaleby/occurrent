3f4c4c1f7cf43f658dad01f884db9e8d3fb67fff
Johan Haleby <johan.haleby@gmail.com>
2023-10-20 15:59:46 +0200

    [ci skip] Fixing issue in release script

--- mvn_release.sh
+++ mvn_release.sh
@@ -9,7 +9,7 @@ mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${re
 mvn release:perform -Prelease && \
 
 echo "Release successful, will update version number for modules that were not included in release build." && \
-git pull --rebase && \
+git pull --rebase # Note that we don't use && here because we may already be up-to-date, and if so, pull returns an "error".
 versionAfterRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
 # We normalize versionAfterRelease by escaping dots with \. This is required for sed to work in find.
 normalizedVersionBeforeRelease=$(echo "${versionBeforeRelease}" | sed 's/\./\\./g') && \
