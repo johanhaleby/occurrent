0d0965fd1f81b371ac9b3fa630e2b58ebe1f2002
Johan Haleby <johan.haleby@gmail.com>
2023-12-28 08:16:44 +0100

    Renamed <T> to <E> in Subscriptions

--- dsl/subscription-dsl/blocking/src/main/kotlin/org/occurrent/dsl/subscription/blocking/Subscriptions.kt
+++ dsl/subscription-dsl/blocking/src/main/kotlin/org/occurrent/dsl/subscription/blocking/Subscriptions.kt
@@ -43,48 +43,47 @@ import kotlin.reflect.KClass
  *
  * This will create a subscription with id "subscriptionId" and subscribe to all events of type "MyEvent" (it uses the [cloudEventConverter] to derive the cloud event type from the domain event type).
  */
-fun <T : Any> subscriptions(subscriptionModel: Subscribable, cloudEventConverter: CloudEventConverter<T>, subscriptions: Subscriptions<T>.() -> Unit) {
+fun <E : Any> subscriptions(subscriptionModel: Subscribable, cloudEventConverter: CloudEventConverter<E>, subscriptions: Subscriptions<E>.() -> Unit) {
     Subscriptions(subscriptionModel, cloudEventConverter).apply(subscriptions)
 }
 
 
-class Subscriptions<T : Any>(private val subscriptionModel: Subscribable, private val cloudEventConverter: CloudEventConverter<T>) {
+class Subscriptions<E : Any>(private val subscriptionModel: Subscribable, private val cloudEventConverter: CloudEventConverter<E>) {
 
     /**
      * Create a new subscription that is invoked after a specific domain event is written to the event store
      */
     @JvmName("subscribeAll")
-    fun subscribe(subscriptionId: String, startAt: StartAt? = null, fn: (T) -> Unit): Subscription {
+    fun subscribe(subscriptionId: String, startAt: StartAt? = null, fn: (E) -> Unit): Subscription {
         return subscribe(subscriptionId, *emptyArray(), startAt = startAt) { e -> fn(e) }
     }
 
     /**
      * Create a new subscription that is invoked after a specific domain event is written to the event store
      */
-    inline fun <reified E : T> subscribe(subscriptionId: String = E::class.simpleName!!, startAt: StartAt? = null, crossinline fn: (E) -> Unit): Subscription {
-        return subscribe(subscriptionId, E::class, startAt = startAt) { e -> fn(e as E) }
+    inline fun <reified E1 : E> subscribe(subscriptionId: String = E1::class.simpleName!!, startAt: StartAt? = null, crossinline fn: (E1) -> Unit): Subscription {
+        return subscribe(subscriptionId, E1::class, startAt = startAt) { e -> fn(e as E1) }
     }
 
 
     @JvmName("subscribeAnyOf")
-    inline fun <reified E1 : T, reified E2 : T> subscribe(subscriptionId: String, startAt: StartAt? = null, crossinline fn: (T) -> Unit): Subscription {
+    inline fun <reified E1 : E, reified E2 : E> subscribe(subscriptionId: String, startAt: StartAt? = null, crossinline fn: (E) -> Unit): Subscription {
         return subscribe(subscriptionId, E1::class, E2::class, startAt = startAt) { e -> fn(e) }
     }
 
     @JvmOverloads
-    fun <E : T> subscribe(subscriptionId: String, eventType: Class<E>, startAt: StartAt? = null, fn: Consumer<E>): Subscription {
+    fun <E1 : E> subscribe(subscriptionId: String, eventType: Class<E>, startAt: StartAt? = null, fn: Consumer<E>): Subscription {
         return subscribe(subscriptionId, listOf(eventType), startAt) { e ->
-            @Suppress("UNCHECKED_CAST")
-            fn.accept(e as E)
+            fn.accept(e)
         }
     }
 
     @JvmOverloads
-    fun subscribe(subscriptionId: String, eventTypes: List<Class<out T>>, startAt: StartAt? = null, fn: Consumer<T>): Subscription {
+    fun subscribe(subscriptionId: String, eventTypes: List<Class<out E>>, startAt: StartAt? = null, fn: Consumer<E>): Subscription {
         return subscribe(subscriptionId, *eventTypes.map { c -> c.kotlin }.toTypedArray(), startAt = startAt) { e -> fn.accept(e) }
     }
 
-    fun subscribe(subscriptionId: String, vararg eventTypes: KClass<out T>, startAt: StartAt? = null, fn: (T) -> Unit): Subscription {
+    fun subscribe(subscriptionId: String, vararg eventTypes: KClass<out E>, startAt: StartAt? = null, fn: (E) -> Unit): Subscription {
         val condition = when {
             eventTypes.isEmpty() -> null
             eventTypes.size == 1 -> Condition.eq(cloudEventConverter[eventTypes[0]])
@@ -94,7 +93,7 @@ class Subscriptions<T : Any>(private val subscriptionModel: Subscribable, privat
         return subscribe(subscriptionId, filter, startAt, fn)
     }
 
-    fun subscribe(subscriptionId: String, filter: OccurrentSubscriptionFilter = OccurrentSubscriptionFilter.filter(Filter.all()), startAt: StartAt? = null, fn: (T) -> Unit): Subscription {
+    fun subscribe(subscriptionId: String, filter: OccurrentSubscriptionFilter = OccurrentSubscriptionFilter.filter(Filter.all()), startAt: StartAt? = null, fn: (E) -> Unit): Subscription {
         val consumer: (CloudEvent) -> Unit = { cloudEvent ->
             val event = cloudEventConverter[cloudEvent]
             fn(event)
