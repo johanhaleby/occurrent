bfb45f5c8af4346c34842d418682ba86771e9df1
Johan Haleby <johan.haleby@gmail.com>
2024-01-19 16:09:52 +0100

    [ci skip] Minor tweak

--- mvn_release.sh
+++ mvn_release.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "!!!!!DON'T FORGET TO SWITCH TO JAVA 17!!!!!"
 read -r -p "Enter the version to release: " releaseVersion
-echo
 read -r -s -p "Enter sonatype password: " sonatypePassword
 echo "Starting to release Occurrent $releaseVersion"
 
