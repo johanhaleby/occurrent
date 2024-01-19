43a19a8e1b6d76a2b7aa175d1a1aaf4e2c3a952d
Johan Haleby <johan.haleby@gmail.com>
2024-01-19 16:37:48 +0100

    [ci skip] Minor tweak

--- mvn_release.sh
+++ mvn_release.sh
@@ -12,8 +12,7 @@ mavenReleaseStatus=$?
 
 if [ $mavenReleaseStatus -eq 0 ]; then
   echo "Release successful, will update version number for modules that were not included in release build." && \
-  # Note that we don't use && here because we may already be up-to-date, and if so, pull returns an "error".
-  git pull --rebase && \
+  git pull --rebase # Note that we don't use && here because we may already be up-to-date, and if so, pull returns an "error".
   versionAfterRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
   # We normalize versionAfterRelease by escaping dots with \. This is required for sed to work in find.
   normalizedVersionBeforeRelease=$(echo "${versionBeforeRelease}" | sed 's/\./\\./g') && \
