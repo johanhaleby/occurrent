5075ea26db17d4bb799bcf892cad45174341553e
Johan Haleby <johan.haleby@gmail.com>
2024-01-20 08:05:59 +0100

    [ci skip] Allow providing version number to script instead of prompting, as well as skipping tests during release
    Because of sonatype issues

--- mvn_release.sh
+++ mvn_release.sh
@@ -1,13 +1,61 @@
 #!/bin/bash
+releaseVersion=""
+skipTests="false"
+
+usage="${PROGNAME} [-h] [-st] [-v] -- Release script for Occurrent
+
+where:
+    -h, --help
+        Show this help text
+    -v, --version
+        The version to release (required)
+    -st, --skip-tests
+        Skip tests when releasing, default false"
+
+if [ "$#" -ne 0 ]; then
+	while [ "$#" -gt 0 ]
+	do
+		case "$1" in
+		-h|--help)
+		    echo "$usage"
+			exit 0
+			;;
+		-v|--version)
+			releaseVersion="$2"
+			;;
+    -st|--skip-tests)
+      skipTests="true"
+      ;;
+		--)
+			break
+			;;
+		-*)
+			echo "Invalid option '$1'. Use --help to see the valid options" >&2
+			exit 1
+			;;
+		# an option mvn_cmd, continue
+		*)	;;
+		esac
+		shift
+	done
+fi
+
+if [ -z "${version}" ]; then
+    echo "$usage"
+    exit 1
+fi
+
+echo "Preparing release of Occurrent ${releaseVersion}"
 echo "!!!!!DON'T FORGET TO SWITCH TO JAVA 17!!!!!"
-read -r -e -p "Enter the version to release: " releaseVersion
 read -r -e -s -p "Enter sonatype password: " sonatypePassword
+echo
 echo
 echo "Starting to release Occurrent $releaseVersion"
 
+mavenArguments=-Dgpg.passphrase=${sonatypePassword} -DskipTests=${skipTests}"
 versionBeforeRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
-mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" -Darguments="-Dgpg.passphrase=${sonatypePassword}" && \
-mvn release:perform -Prelease -Darguments="-Dgpg.passphrase=${sonatypePassword}"
+mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" -Darguments="${mavenArguments}" && \
+mvn release:perform -Prelease -Darguments="${mavenArguments}"
 mavenReleaseStatus=$?
 
 if [ $mavenReleaseStatus -eq 0 ]; then
