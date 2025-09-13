cd3cc5f8f26c2a4db92a10a49a6052e40b2fdf51
Kirill Gavrilov <gavvvr@users.noreply.github.com>
2025-09-14 02:31:20 +0300

    Simplify release script after removal oh hard-coded values

--- mvn_release.sh
+++ mvn_release.sh
@@ -53,27 +53,12 @@ echo
 echo
 echo "Starting to release Occurrent $releaseVersion (skip tests=$skipTests)"
 
-mavenArguments="-Dgpg.passphrase=${sonatypePassword} -DskipTests=${skipTests}"
-versionBeforeRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
-mvn release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="occurrent-${releaseVersion}" -DreleaseVersion="${releaseVersion}" -Darguments="${mavenArguments}" && \
-mvn release:perform -Prelease -Darguments="-Dgpg.passphrase=${sonatypePassword} -Dmaven.test.skip=true" && \
-git checkout occurrent-${releaseVersion} && \
-mvn deploy -Prelease -DskipTests=true
+mvn deploy -Prelease -DskipTests=true -Drevision=${releaseVersion}
 mavenReleaseStatus=$?
 
 git checkout master
 
-if [ $mavenReleaseStatus -eq 0 ]; then
-  echo "Release successful, will update version number for modules that were not included in release build." && \
-  git pull --rebase 2>/dev/null || true # Note that we don't use && here because we may already be up-to-date, and if so, pull returns an "error".
-  versionAfterRelease=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout) && \
-  # We normalize versionAfterRelease by escaping dots with \. This is required for sed to work in find.
-  normalizedVersionBeforeRelease=$(echo "${versionBeforeRelease}" | sed 's/\./\\./g') && \
-  find . -name "pom.xml" -type f -exec sed -i "" "s/${normalizedVersionBeforeRelease}/${versionAfterRelease}/g" {} + && \
-  git commit -am "Updated version number for modules that were not included in release build to ${versionAfterRelease}" && \
-  git push && \
-  echo "Maven release of Occurrent $releaseVersion completed successfully"
-else
+if [ $mavenReleaseStatus -ne 0 ]; then
   echo "Maven release of Occurrent $releaseVersion failed"
   exit $mavenReleaseStatus
 fi
