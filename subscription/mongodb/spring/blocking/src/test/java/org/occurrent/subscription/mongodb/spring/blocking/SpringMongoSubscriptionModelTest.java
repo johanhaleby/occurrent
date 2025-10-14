24139895edc401fb0d322cb7f427028020066af6
Johan Haleby <johan.haleby@gmail.com>
2025-10-14 15:42:56 +0200

    Minor refactoring

--- subscription/mongodb/spring/blocking/src/test/java/org/occurrent/subscription/mongodb/spring/blocking/SpringMongoSubscriptionModelTest.java
+++ subscription/mongodb/spring/blocking/src/test/java/org/occurrent/subscription/mongodb/spring/blocking/SpringMongoSubscriptionModelTest.java
@@ -91,8 +91,7 @@ import static org.occurrent.subscription.mongodb.spring.blocking.SpringMongoSubs
 public class SpringMongoSubscriptionModelTest {
 
     @Container
-    private static final MongoDBContainer mongoDBContainer =
-            new MongoDBContainer("mongo:" + System.getProperty("test.mongo.version")).withReuse(true);
+    private static final MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:" + System.getProperty("test.mongo.version")).withReuse(true);
     private static final String RESUME_TOKEN_COLLECTION = "ack";
 
     @RegisterExtension
