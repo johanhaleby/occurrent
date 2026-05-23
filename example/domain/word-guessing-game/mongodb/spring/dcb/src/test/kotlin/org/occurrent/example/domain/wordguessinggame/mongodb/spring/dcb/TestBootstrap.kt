de4a4ff81b67b1a164356c3ab2d6eb38a2175408
Johan Haleby <johan.haleby@gmail.com>
2026-05-23 17:08:49 +0200

    Implement manual DCB bootstrap

---
+++ example/domain/word-guessing-game/mongodb/spring/dcb/src/test/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/dcb/TestBootstrap.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2026 Johan Haleby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.occurrent.example.domain.wordguessinggame.mongodb.spring.dcb
+
+import org.occurrent.example.domain.wordguessinggame.mongodb.spring.dcb.features.gameplay.usecases.MakeGuess
+import org.occurrent.example.domain.wordguessinggame.mongodb.spring.dcb.features.gameplay.usecases.StartGame
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.context.annotation.Bean
+import org.testcontainers.mongodb.MongoDBContainer
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestBootstrap {
+
+    @Bean
+    @ServiceConnection
+    fun mongoDbContainer(): MongoDBContainer = MongoDBContainer("mongo:4.2.8").withReplicaSet()
+
+    @Bean
+    fun startGame(): StartGame = StartGame { _, _, _, _ -> }
+
+    @Bean
+    fun makeGuess(): MakeGuess = MakeGuess { _, _, _, _ -> }
+}
