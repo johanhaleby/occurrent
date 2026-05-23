2629d6708ee5816beaaa5a6bb07b257d58b5b2f2
Johan Haleby <johan.haleby@gmail.com>
2026-05-23 17:22:38 +0200

    Implement manual DCB commands and policies

---
+++ example/domain/word-guessing-game/mongodb/spring/dcb/src/main/kotlin/org/occurrent/example/domain/wordguessinggame/mongodb/spring/dcb/features/wordhint/RevealInitialCharactersInWordHintAfterGameIsStarted.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2026 Johan Haleby
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.occurrent.example.domain.wordguessinggame.mongodb.spring.dcb.features.wordhint
+
+import org.occurrent.application.service.blocking.dcb.DcbApplicationService
+import org.occurrent.dsl.subscription.blocking.Subscriptions
+import org.occurrent.example.domain.wordguessinggame.event.CharacterInWordHintWasRevealed
+import org.occurrent.example.domain.wordguessinggame.event.GameEvent
+import org.occurrent.example.domain.wordguessinggame.event.GameWasStarted
+import org.occurrent.example.domain.wordguessinggame.mongodb.spring.dcb.features.dcb.GameDcbQueries
+import org.occurrent.example.domain.wordguessinggame.writemodel.WordHintCharacterRevelation
+import org.occurrent.example.domain.wordguessinggame.writemodel.WordHintData
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import java.util.stream.Stream
+import kotlin.streams.asStream
+
+@Configuration
+class RevealInitialCharactersInWordHintAfterGameIsStarted(
+    private val applicationService: DcbApplicationService<GameEvent>,
+    private val subscriptions: Subscriptions<GameEvent>
+) {
+
+    @Bean
+    fun whenGameWasStartedThenRevealInitialCharactersInWordHint() =
+        subscriptions.subscribe<GameWasStarted>("WhenGameWasStartedThenRevealInitialCharactersInWordHintPolicy") { gameWasStarted ->
+            invoke(gameWasStarted)
+        }
+
+    @Retryable(backoff = Backoff(delay = 100, multiplier = 2.0, maxDelay = 1000))
+    operator fun invoke(gameWasStarted: GameWasStarted) {
+        applicationService.execute(GameDcbQueries.wordHintDecisionContext(gameWasStarted.gameId)) { events: Stream<GameEvent> ->
+            if (events.toList().filterIsInstance<CharacterInWordHintWasRevealed>().isEmpty()) {
+                WordHintCharacterRevelation
+                    .revealInitialCharactersInWordHintWhenGameWasStarted(WordHintData(gameWasStarted.gameId, gameWasStarted.wordToGuess))
+                    .asStream()
+            } else {
+                Stream.empty()
+            }
+        }
+    }
+}
